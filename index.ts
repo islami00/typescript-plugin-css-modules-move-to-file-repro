b5ce73a40aac00e2a9f42fd717ab4be12828874b
Islam <islamkwnd@gmail.com>
2023-10-21 22:19:22 +0100

    feat: initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+import "./example.d.ts";
+// commenting this or removing the plugin allows move to file to work
+import classes from "./mod.module.scss";
+/*
+  ^-: (alias) let classes: {
+    a: string;
+  }
+*/
---
+++ new-file.tsx
@@ -0,0 +1,2 @@
+// try moving this to index.ts in vscode
+const a = 0;
